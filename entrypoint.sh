29fa447d1aa2bb44ada04df4bdab5882f125466f
Mindaugas Laganeckas <mindaugas.laganeckas@3shape.com>
2020-04-22 15:06:41 +0200

    Add debug print

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 INPUT_FOLEDER=${args[0]}
-DIAGRAM_FILE_EXTENSION=".mmd"
+DIAGRAM_FILE_EXTENSION="mmd"
 
 cd $INPUT_FOLEDER
-for infile in *$DIAGRAM_FILE_EXTENSION ; do
-    fbname=$(basename "$infile" $DIAGRAM_FILE_EXTENSION)
+ls
+for infile in *.$DIAGRAM_FILE_EXTENSION ; do
+    fbname=$(basename "$infile" .$DIAGRAM_FILE_EXTENSION)
     /usr/local/bin/mmdc -p puppeteer-config.json -i $infile -o $fbname.svg
 done
 
