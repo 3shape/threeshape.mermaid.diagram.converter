fb7f5743df93cc16384cf13463edcc7324d418b3
Mindaugas Laganeckas <mindaugas.laganeckas@3shape.com>
2020-04-22 15:00:29 +0200

    Yet another syntax mistake

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ INPUT_FOLEDER=${args[0]}
 DIAGRAM_FILE_EXTENSION=".mmd"
 
 cd $INPUT_FOLEDER
-for infile in *.$DIAGRAM_FILE_EXTENSION ; do
+for infile in *$DIAGRAM_FILE_EXTENSION ; do
     fbname=$(basename "$infile" $DIAGRAM_FILE_EXTENSION)
     /usr/local/bin/mmdc -p puppeteer-config.json -i $infile -o $fbname.svg
 done
