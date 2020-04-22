503da8dfb99be037ae7315e58fbf649b370ab55a
Mindaugas Laganeckas <mindaugas.laganeckas@3shape.com>
2020-04-22 15:18:57 +0200

    Fix input argument error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-INPUT_FOLEDER=${args[0]}
+INPUT_FOLDER=$1
 DIAGRAM_FILE_EXTENSION="mmd"
 
-cd $INPUT_FOLEDER
+cd $INPUT_FOLDER
 ls
-for infile in *.$DIAGRAM_FILE_EXTENSION ; do
+for infile in *.$DIAGRAM_FILE_EXTENSION; do
     fbname=$(basename "$infile" .$DIAGRAM_FILE_EXTENSION)
     /usr/local/bin/mmdc -p puppeteer-config.json -i $infile -o $fbname.svg
 done
